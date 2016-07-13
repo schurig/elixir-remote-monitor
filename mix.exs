9d85995c7020e61e9c8a02946e8c0dd359112aa2
Martin Schurig <martin@schurig.pw>
2016-07-13 18:20:47 +0200

    Bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule RemoteMonitor.Mixfile do
 
   def project do
     [app: :remote_monitor,
-     version: "0.1.2",
+     version: "0.2.0",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
